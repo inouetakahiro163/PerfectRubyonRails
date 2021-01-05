717da4fdd1f4536690cf29e72b7f363e201f6bbd
nobita <inouetakahiro163@gmail.com>
2021-01-05 10:42:15 +0900

    ルーティングresouces resourceを使用したルーティングを設定 only:オプションでアクションを限定する

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/books/:id' => 'books#show'
   delete '/books/:id' => 'books#destroy'
+  resources :publishers
+  # 単数リソースを設定する　=>　コントローラが扱うリソースが一つの場合（ログインユーザーのプロフィールなど）
+  # routesを設定するactionを限定する　only:オプション
+  resource :profile, only: %i{show edit update}
 end
