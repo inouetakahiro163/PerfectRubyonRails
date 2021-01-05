0e15476d7be3f32926b95f0bc98441ad07928daa
nobita <inouetakahiro163@gmail.com>
2021-01-05 11:34:55 +0900

    add: strongparameterを使用して許可したキーのみ利用する

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,5 +6,18 @@ class ProfilesController < ApplicationController
   end
 
   def update
+    user = current_user
+    user.update(user_params)
+  end
+
+  # マスアサインメントでハッシュ形式のデータを一括で設定する際にパラメータに予期せぬデータを入力されるのを防ぐ
+  private
+  
+  def user_params
+    if current_user.admin?
+      params.require(:user).permit(:name, :email, :admin)
+    else
+      params.require(:user).permit(:name, :email)
+    end
   end
 end
